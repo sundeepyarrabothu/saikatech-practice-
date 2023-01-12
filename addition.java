dbcb8c1d4222651e4e88d9c6f6f3badedaf828b1
sundeepyarrabothu <91503267+sundeepyarrabothu@users.noreply.github.com>
2023-01-12 16:02:05 -0600

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,11 @@
+package saika_learnings;
+public class addition {
+	static int a =1;
+	static int b =2;
+	
+  public static void main(String[] args) {
+	  int c= a+b;
+    System.out.println(c);
+  }
+}
+
