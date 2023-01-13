c3a96d17a39d5607a16888b7b414d751cbb4c7c3
sundeepyarrabothu <91503267+sundeepyarrabothu@users.noreply.github.com>
2023-01-13 14:16:52 -0600

    Add files via upload
    Arithmetic Operations

---
+++ arithmaticOperations.java
@@ -0,0 +1,18 @@
+package saika_learnings;
+
+public  class arithmaticOperations{
+
+	public  static void main(String []args) {
+	
+	
+	 int a = 10;
+	 int b = 5;
+	 
+	 System.out.println(a+b);
+	 System.out.println(a-b);
+	 System.out.println(a*b);
+	 System.out.println(a%b);
+	
+	}
+	
+}
